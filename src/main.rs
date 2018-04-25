c9e61e9f1c19dccc262718fbfdbf3fab6f3286cf
Gregory <gregory.mkv@gmail.com>
2018-04-25 16:38:23 -0400

    main.rs - fix operators

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     // let expr = parse_expr("1*2*3 + 24*5 - 10");
     // println!("{:?}", expr);
     // println!("{}", &expr.eval());
-    println!("{:?}", parser::parse("3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2 ^ 3"));
+    println!("{:?}", parser::parse("3 + 4 * 2 / ( 1 − 5 ) ^ 2 ^ 3"));
 }
