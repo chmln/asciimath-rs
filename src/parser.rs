4f1af3af5ae24e231140cd02aaa993964f8d3586
Gregory <gregory.mkv@gmail.com>
2018-04-30 12:16:28 -0400

    Parser - refactor

--- src/parser.rs
+++ src/parser.rs
@@ -2,165 +2,136 @@ use lexer::tokenize;
 use std::collections::VecDeque;
 
 use ast::Node;
-use tokens::Token;
+use logger::log;
+use tokens::{Function, Operator, Token};
 
 pub fn parse(expr: &str) -> Result<Node, String> {
     parse_tokens(tokenize(expr))
 }
 
-// TODO: break this down
-pub fn parse_tokens(tokens: VecDeque<Token>) -> Result<Node, String> {
-    let mut operator_stack: Vec<Token> = Vec::new();
-    let mut operand_stack: Vec<Node> = Vec::new();
+fn make_node(token: Token, args: Option<VecDeque<Node>>) -> Node {
+    Node { token, args }
+}
 
-    let add_node = |token, args| Node {
-        token,
-        args,
-        // lhs: Some(Box::new(lhs)),
-        // rhs: Some(Box::new(rhs)),
-    };
+fn encounter_func(f: Function, operands: &mut Vec<Node>) {
+    let mut args: VecDeque<Node> = VecDeque::with_capacity(2);
+    args.push_front(operands.pop().unwrap());
+
+    while !operands.is_empty() {
+        let last = operands.pop().unwrap();
+        if last.token != Token::Comma {
+            operands.push(last);
+            break;
+        }
+        else {
+            args.push_front(operands.pop().unwrap());
+        }
+    }
+
+    operands.push(make_node(Token::Function(f), Some(args)));
+}
+
+pub fn right_paren(operators: &mut Vec<Token>, operands: &mut Vec<Node>) {
+    while !operators.is_empty() {
+        let top = operators.pop().unwrap();
+        match top {
+            Token::LeftParenthesis => {
+                break;
+            },
+            Token::Function(f) => encounter_func(f, operands),
+            Token::Operator(op) => {
+                let rhs = operands.pop().expect("missing operand");
+                let lhs = operands.pop().expect("missing operand");
+                operands.push(make_node(
+                    Token::Operator(op),
+                    Some(VecDeque::from(vec![lhs, rhs])),
+                ));
+            },
+            _ => {},
+        }
+    }
+}
+
+pub fn encounter_operator(
+    op1: Operator,
+    operators: &mut Vec<Token>,
+    operands: &mut Vec<Node>,
+) {
+    while !operators.is_empty() {
+        let top = operators.pop().unwrap();
+
+        match top {
+            Token::Operator(top_op) => if top_op > op1
+                || (top_op == op1 && !op1.is_right_associative())
+            {
+                let rhs = operands.pop().unwrap();
+                let lhs = operands.pop().unwrap();
+
+                operands.push(make_node(
+                    Token::Operator(top_op),
+                    Some(VecDeque::from(vec![lhs, rhs])),
+                ));
+            }
+            else {
+                operators.push(Token::Operator(top_op));
+                break;
+            },
+            Token::Function(f) => encounter_func(f, operands),
+            _ => {
+                operators.push(top);
+                break;
+            },
+        }
+    }
+
+    log(format!("Push op to stack: {:?}", op1));
+    operators.push(Token::Operator(op1));
+}
+
+pub fn parse_tokens(tokens: VecDeque<Token>) -> Result<Node, String> {
+    let mut operators: Vec<Token> = Vec::new();
+    let mut operands: Vec<Node> = Vec::new();
 
     for token in tokens {
-        println!("TOKEN: {:?}", token);
+        log(format!("TOKEN: {:?}", token));
         match token {
             Token::Number(num) => {
-                operand_stack.push(Node {
+                operands.push(Node {
                     token: Token::Number(num),
                     args: None,
                 });
-                println!("Add number to output: ");
-            }
-            Token::Variable(var) => operand_stack.push(Node {
+                log(format!("Add number to output: "));
+            },
+            Token::Variable(var) => operands.push(Node {
                 token: Token::Variable(var),
                 args: None,
             }),
-            Token::RightParenthesis => {
-                while !operator_stack.is_empty() {
-                    let top = operator_stack.pop().unwrap();
-                    match top {
-                        Token::LeftParenthesis => {
-                            break;
-                        }
-                        Token::Function(f) => {
-                            let mut args: Vec<Node> = Vec::new();
-                            args.push(operand_stack.pop().unwrap());
-                            //let last = ;
+            Token::RightParenthesis =>
+                right_paren(&mut operators, &mut operands),
+            Token::LeftParenthesis => operators.push(token),
 
-                            while !operand_stack.is_empty() {
-                                let last = operand_stack.pop().unwrap();
-                                if last.token != Token::Comma {
-                                    operand_stack.push(last);
-                                    break;
-                                }
-                                else {
-                                    args.push(operand_stack.pop().unwrap());
-                                }
-                            }
+            Token::Operator(op1) =>
+                encounter_operator(op1, &mut operators, &mut operands),
 
-                            //let lhs = operand_stack.pop().unwrap();
-                            //println!("{:?} {:?} {:?}", lhs, f, rhs);
-                            operand_stack
-                                .push(add_node(Token::Function(f), Some(args)));
-                        }
-                        _ => {
-                            let rhs =
-                                operand_stack.pop().expect("missing operand");
-                            let lhs =
-                                operand_stack.pop().expect("missing operand");
-                            operand_stack
-                                .push(add_node(top, Some(vec![lhs, rhs])));
-                        }
-                    }
-                }
-            }
-
-            Token::LeftParenthesis => {
-                operator_stack.push(token);
-            }
-
-            Token::Operator(op1) => {
-                while !operator_stack.is_empty() {
-                    let top = operator_stack.pop().unwrap();
-
-                    match top {
-                        Token::Operator(top_operator) => {
-                            if top_operator > op1
-                                || (top_operator == op1
-                                    && !op1.is_right_associative())
-                            {
-                                let rhs = operand_stack.pop().unwrap();
-                                let lhs = operand_stack.pop().unwrap();
-                                //println!("{:?} {:?} {:?}", lhs, top_operator, rhs);
-                                operand_stack.push(add_node(
-                                    Token::Operator(top_operator),
-                                    Some(vec![lhs, rhs]),
-                                ));
-                            }
-                            else {
-                                operator_stack
-                                    .push(Token::Operator(top_operator));
-                                break;
-                            }
-                        }
-                        Token::Function(f) => {
-                            let mut args: Vec<Node> = Vec::new();
-                            args.push(operand_stack.pop().unwrap());
-                            //let last = ;
-
-                            while !operand_stack.is_empty() {
-                                let last = operand_stack.pop().unwrap();
-                                if last.token != Token::Comma {
-                                    operand_stack.push(last);
-                                    break;
-                                }
-                                else {
-                                    args.push(operand_stack.pop().unwrap());
-                                }
-                            }
-
-                            //let lhs = operand_stack.pop().unwrap();
-                            //println!("{:?} {:?} {:?}", lhs, f, rhs);
-                            operand_stack
-                                .push(add_node(Token::Function(f), Some(args)));
-                        }
-                        _ => {
-                            operator_stack.push(top);
-                            break;
-                            //println!("{:?}", top);
-                        }
-                    }
-                }
-
-                println!("Push op to stack: {:?}", op1);
-                operator_stack.push(Token::Operator(op1));
-            }
-            Token::Function(f) => {
-                println!("Push func to stack: {:?}", f);
-                operator_stack.push(Token::Function(f))
-            }
-            Token::Comma => {
-                operand_stack.push(add_node(token, None));
-            }
+            Token::Function(f) => operators.push(Token::Function(f)),
+            Token::Comma => operands.push(make_node(token, None)),
         };
-        println!("stack: {:?}", operator_stack);
-        println!("output: {:?}", operand_stack);
-        println!("----------");
+        log(format!("stack: {:?}", operators));
+        log(format!("output: {:?}", operands));
+        log(format!("----------"));
     }
 
-    while !operator_stack.is_empty() {
-        let rhs = operand_stack.pop().unwrap();
-        let lhs = operand_stack.pop().unwrap();
-        let operator = operator_stack.pop().unwrap();
-        operand_stack.push(add_node(operator, Some(vec![lhs, rhs])));
+    while !operators.is_empty() {
+        let rhs = operands.pop().unwrap();
+        let lhs = operands.pop().unwrap();
+        let operator = operators.pop().unwrap();
+        operands.push(make_node(
+            operator,
+            Some(VecDeque::from(vec![lhs, rhs])),
+        ));
     }
 
-    //println!("{:?}", operand_stack);
-
-    if let Some(node) = operand_stack.pop() {
-        //println!("ROOT: {:?}", node);
-        Ok(node)
-    }
-    else {
-        Err("empty expression".to_string())
-    }
+    // TODO: revisit this when the final output is a string/whatever, not just
+    // a float
+    Ok(operands.pop().unwrap())
 }
