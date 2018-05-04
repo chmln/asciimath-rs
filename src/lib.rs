600e728778d75f0a27e0309fef6b40dca41af6c1
Gregory <gregory.mkv@gmail.com>
2018-05-04 16:18:55 -0400

    Document custom functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,39 @@
 //! A mathematical expression parsing and evaluation library.
 //!
-//! # Typical Use
+//! # Get Started
+//!
+//! The example below demonstrates parsing and evaluation of an expression
+//! with user-defined variables. The compiled expression is immutable and can be evaluated with many scopes.
 //!
 //! ```
 //! extern crate asciimath;
 //! use asciimath::{parse,Scope,Evaluate};
 //!
-//! let parsed_expr = parse("(x+y*4)^3").unwrap();
+//! let parsed_expr = parse("(x + y * 4) ^ 3").unwrap();
 //! let mut scope = Scope::new();
 //! scope.set_var("x", 8);
 //! scope.set_var("y", 12.25);
 //!
 //! assert_eq!(Ok(185193.0), parsed_expr.eval_with(&scope));
 //! ```
+//!
+//! # Custom Functions
+//!
+//! ```
+//! extern crate asciimath;
+//! use asciimath::{parse,Scope,Evaluate,CustomFn};
+//!
+//! let parsed_expr = parse("my_sum(x, 2, 3)").unwrap();
+//! let my_sum: CustomFn = |args| Ok(args.iter().sum());
+//!
+//! let mut scope = Scope::new();
+//! scope.set_var("x", 1);
+//! scope.set_var("my_sum", my_sum);
+//!
+//! assert_eq!(Ok(6.0), parsed_expr.eval_with(&scope));
+//! ```
+//!
+//!
 
 #[macro_use]
 extern crate lazy_static;
