8ca43c7f4680121c558b822bd7849e6a09b05b1b
Gregory <gregory.mkv@gmail.com>
2018-05-04 02:20:14 -0400

    Builtin functions - simplify

--- src/functions/index.rs
+++ src/functions/index.rs
@@ -9,16 +9,17 @@ lazy_static! {
     pub static ref FUNCTIONS: HashMap<&'static str, Func> = {
         let mut m = HashMap::with_capacity(10);
 
+
         let sin  = |args: &Args| Ok(args[0].to_radians().sin());
         let cos  = |args: &Args| Ok(args[0].to_radians().cos());
         let tan  = |args: &Args| Ok(args[0].to_radians().tan());
 
         let max =  |args: &Args| Ok(args.iter().fold(0. / 0., |acc: NumericLiteral, x| acc.max(*x)));
         let min  =  |args: &Args| Ok(args.iter().fold(0. / 0., |acc: NumericLiteral, x| acc.min(*x)));
-        let abs  = |args: &Args| Ok(args.get(0).unwrap().abs());
+        let abs  = |args: &Args| Ok(args[0].abs());
 
-        let sqrt = |args: &Args| Ok(args.get(0).unwrap().sqrt());
-        let cbrt  = |args: &Args| Ok(args.get(0).unwrap().cbrt());
+        let sqrt = |args: &Args| Ok(args[0].sqrt());
+        let cbrt  = |args: &Args| Ok(args[0].cbrt());
 
         // comparison
         m.insert("min", min as Func);
