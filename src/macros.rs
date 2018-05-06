7fa8258acd8484a44d74fa1c43ce834900ea5e8b
Gregory <gregory.mkv@gmail.com>
2018-05-05 22:12:43 -0400

    Simplify debug! macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,9 @@
-#[cfg(debug_assertions)]
 macro_rules! debug {
-    ($( $args:expr ),*) => { println!( $( $args ),* ); }
-}
-
-#[cfg(not(debug_assertions))]
-macro_rules! debug {
-    ($($args:expr),*) => {};
+    ($( $args:expr ),*) => {
+        if cfg!(debug_assertions) {
+            println!( $( $args ),* );
+        }
+    }
 }
 
 // TODO: micro-optimizations / use with_capacity() somehow
