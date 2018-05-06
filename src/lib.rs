0eae9589a1faf938dddffc12eb77787cc4a0b273
Gregory <gregory.mkv@gmail.com>
2018-05-06 02:30:08 -0400

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,20 +9,22 @@
 //! #[macro_use] extern crate asciimath;
 //! use asciimath::eval;
 //!
-//! assert_eq!(Ok(185193.0), eval("(x + y * 4) ^ 3", &scope!{
+//! let expression = "(x + y * 4) ^ 3";
+//! let variables = scope!{
 //!    "x" => 8,
 //!    "y" => 12.25
-//! }));
+//! };
+//!
+//! assert_eq!(Ok(185193.0), eval(expression, &variables));
 //! ```
 //!
-//! For repeated evaluation of an expression, see the next example.
 //!
-//! # Compiling Expressions
+//! # Compiling Expressions for Repeated Evaluation
 //!
 //! The example below demonstrates parsing and evaluation of an expression
 //! with two sets of variables.
 //!
-//! The Scope is passed to the compiler only disambiguation in cases of
+//! The Scope is passed to the compiler for disambiguation in cases of
 //! implicit multiplication and function calls.
 //!
 //! ```
@@ -45,9 +47,12 @@
 //!
 //! # Custom Functions
 //!
+//! A lot of effort has been put into making custom functions as easy to write
+//! as possible.
+//!
 //! ```
 //! #[macro_use] extern crate asciimath;
-//! use asciimath::{eval,Evaluate,CustomFn};
+//! use asciimath::{eval,CustomFn};
 //!
 //! let my_sum: CustomFn = |args| Ok(args.iter().sum());
 //!
@@ -59,7 +64,15 @@
 //! assert_eq!(Ok(6.0), eval("my_sum(x, 2, 3)",&scope));
 //! ```
 //!
+//! # Built-in Functions
 //!
+//! - `cos(x)`
+//! - `tan(x)`
+//! - `max(a,b,c,...)`
+//! - `min(a,b,c,...)`
+//! - `abs(x)`
+//! - `sqrt(x)`
+//! - `cbrt(x)`
 
 #[macro_use]
 extern crate lazy_static;
