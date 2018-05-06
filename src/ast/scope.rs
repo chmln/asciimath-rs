a09c814b05cfbc47df379599ef6624b82c5f239d
Gregory <gregory.mkv@gmail.com>
2018-05-06 16:10:30 -0400

    Move scope! macro to macros.rs

--- src/ast/scope.rs
+++ src/ast/scope.rs
@@ -52,22 +52,3 @@ impl Scope {
         self.variables.get(var_name)
     }
 }
-
-// Thank you, https://github.com/bluss
-#[macro_export]
-macro_rules! scope {
-    (@single $($x:tt)*) => (());
-    (@count $($rest:expr),*) => (<[()]>::len(&[$(scope!(@single $rest)),*]));
-
-    ($($key:expr => $value:expr,)+) => { scope!($($key => $value),+) };
-    ($($key:expr => $value:expr),*) => {
-        {
-            let _cap = scope!(@count $($key),*);
-            let mut _map = $crate::Scope::with_capacity(_cap);
-            $(
-                _map.set_var($key, $value);
-            )*
-            _map
-        }
-    };
-}
--- src/macros.rs
+++ src/macros.rs
@@ -18,3 +18,22 @@ macro_rules! vec_deque {
         }
     };
 }
+
+// Thank you, https://github.com/bluss
+#[macro_export]
+macro_rules! scope {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(scope!(@single $rest)),*]));
+
+    ($($key:expr => $value:expr,)+) => { scope!($($key => $value),+) };
+    ($($key:expr => $value:expr),*) => {
+        {
+            let _cap = scope!(@count $($key),*);
+            let mut _map = $crate::Scope::with_capacity(_cap);
+            $(
+                _map.set_var($key, $value);
+            )*
+            _map
+        }
+    };
+}
